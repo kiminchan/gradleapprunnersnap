eedf1c334dbc1be83218203380565ecf28382e81
kimin <kiminchan12341@gmail.com>
2023-05-10 20:47:22 +0900

    Initial commit

---
+++ src/main/java/com/example/gradleapprunnersnap/GradleapprunnersnapApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradleapprunnersnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleapprunnersnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleapprunnersnapApplication.class, args);
+	}
+
+}
